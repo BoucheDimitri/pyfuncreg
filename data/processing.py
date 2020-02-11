6310cbfc47e4bd074c51929d8b34fcb916d61d93
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2020-02-11 19:43:39 +0100

    Tested everything including on speech

--- data/processing.py
+++ data/processing.py
@@ -2,47 +2,40 @@ import numpy as np
 import python_speech_features
 
 
-def process_dti_dataset(cca, rcst, n_train=70, normalize01=True, interpout=True,
-                        pad_train_input=True, pad_mode_input="symmetric",
-                        pad_width_input=((0, 0), (93, 93)), pad_train_output=True,
-                        pad_mode_output="symmetric", pad_width_output=((0, 0), (55, 55))):
+def process_dti_dataset(cca, rcst, n_train=70, normalize01=True,
+                        pad_mode_input="symmetric", pad_width_input=((0, 0), (0, 0)),
+                        pad_mode_output="symmetric", pad_width_output=((0, 0), (0, 0))):
+    # Evaluation grids dimensions
     dimx, dimy = cca.shape[1], rcst.shape[1]
+    # Number of sample points
     n = cca.shape[0]
+    # Normalization constants
     if normalize01:
         norma_in, norma_out = dimx, dimy
     else:
         norma_in, norma_out = 1, 1
+    # Divide between train and test
     cca_train, cca_test = cca[:n_train, :], cca[n_train:, :]
     rcst_train, rcst_test = rcst[:n_train, :], rcst[n_train:, :]
-    full_input_locs = (1 / norma_in) * np.arange(dimx)
-    full_output_locs = (1 / norma_out) * np.arange(dimy)
-    if pad_train_input:
-        cca_train = np.pad(cca_train, pad_width=pad_width_input, mode=pad_mode_input)
-        full_input_locs_pad = (1 / norma_in) * np.arange(- pad_width_input[1][0], dimx + pad_width_input[1][1])
-    else:
-        full_input_locs_pad = full_input_locs
-    if pad_train_output:
-        rcst_train = np.pad(rcst_train, pad_width=pad_width_output, mode=pad_mode_output)
-        full_output_locs_pad = (1 / norma_out) * np.arange(- pad_width_output[1][0], dimy + pad_width_output[1][1])
-    else:
-        full_output_locs_pad = full_output_locs
+    # Pad training inputs
+    cca_train = np.pad(cca_train, pad_width=pad_width_input, mode=pad_mode_input)
+    cca_test = np.pad(cca_test, pad_width=pad_width_input, mode=pad_mode_input)
+    full_input_locs_pad = (1 / norma_in) * np.arange(-pad_width_input[1][0], dimx + pad_width_input[1][1])
+    # Pad training outputs
+    rcst_train = np.pad(rcst_train, pad_width=pad_width_output, mode=pad_mode_output)
+    # Initialize containers
     Xtrain_locs, Xtrain_evals, Ytrain_locs, Ytrain_evals = [], [], [], []
     Xtest_locs, Xtest_evals, Ytest_locs, Ytest_evals = [], [], [], []
     for i in range(n_train):
-        if interpout:
-            ylocs = (1 / norma_out) * (np.argwhere(~ np.isnan(rcst_train[i])).squeeze() - pad_width_output[1][0])
-            Ytrain_locs.append(full_output_locs_pad)
-            Ytrain_evals.append(np.interp(full_output_locs_pad, ylocs, rcst_train[i][~ np.isnan(rcst_train[i])]))
-        else:
-            Ytrain_evals.append(rcst_train[i][~ np.isnan(rcst_train[i])])
-            Ytrain_locs.append((1 / norma_out) * (np.argwhere(~ np.isnan(rcst_train[i])).squeeze() - pad_width_output[1][0]))
         xlocs = (1 / norma_in) * (np.argwhere(~ np.isnan(cca_train[i])).squeeze() - pad_width_input[1][0])
         Xtrain_locs.append(full_input_locs_pad)
         Xtrain_evals.append(np.interp(full_input_locs_pad, xlocs, cca_train[i][~ np.isnan(cca_train[i])]))
+        Ytrain_evals.append(rcst_train[i][~ np.isnan(rcst_train[i])])
+        Ytrain_locs.append((1 / norma_out) * (np.argwhere(~ np.isnan(rcst_train[i])).squeeze() - pad_width_output[1][0]))
     for i in range(n - n_train):
-        xlocs = (1 / norma_in) * np.argwhere(~ np.isnan(cca_test[i])).squeeze()
-        Xtest_locs.append(full_input_locs)
-        Xtest_evals.append(np.interp(full_input_locs, xlocs, cca_test[i][~ np.isnan(cca_test[i])]))
+        xlocs = (1 / norma_in) * (np.argwhere(~ np.isnan(cca_test[i])).squeeze() - pad_width_input[1][0])
+        Xtest_locs.append(full_input_locs_pad)
+        Xtest_evals.append(np.interp(full_input_locs_pad, xlocs, cca_test[i][~ np.isnan(cca_test[i])]))
         Ytest_evals.append(rcst_test[i][~ np.isnan(rcst_test[i])])
         Ytest_locs.append((1 / norma_out) * np.argwhere(~ np.isnan(rcst_test[i])).squeeze())
     return (Xtrain_locs, Xtrain_evals), (Ytrain_locs, Ytrain_evals), (Xtest_locs, Xtest_evals), (Ytest_locs, Ytest_evals)
@@ -86,8 +79,6 @@ def process_dti_bis(cca, rcst, n_train=80, normalize01=True, interpout=True,
         Ytest_evals.append(rcst_test[i][~ np.isnan(rcst_test[i])])
         Ytest_locs.append((1 / norma_out) * np.argwhere(~ np.isnan(rcst_test[i])).squeeze())
     return (Xtrain_locs, Xtrain_evals), (Ytrain_locs, Ytrain_evals), (Xtest_locs, Xtest_evals), (Ytest_locs, Ytest_evals)
-
-
 
 
 def process_speech_dataset(X, Y, duration="max", pad_mode="symmetric",
--- tests/test_dti.py
+++ tests/test_dti.py
@@ -11,20 +11,43 @@ N_TRAIN = 70
 # ############################ Load DTI data ###########################################################################
 cca, rcst = loading.load_dti(os.getcwd() + "/data/dataDTI/", shuffle_seed=SHUFFLE_SEED)
 
+Xtrain0, Ytrain0, Xtest0, Ytest0 = processing.process_dti_bis(cca.copy(), rcst.copy(), n_train=N_TRAIN,
+                                                              normalize01=True, interpout=False, pad_train=True,
+                                                          pad_width=((0, 0), (0, 0)), pad_mode="symmetric")
 
+Xtrain1, Ytrain1, Xtest1, Ytest1 = processing.process_dti_dataset(cca.copy(), rcst.copy(), n_train=N_TRAIN,
+                                                                  normalize01=True, interpout=False,
+                                                                  pad_train_output=True, pad_train_input=True,
+                                                                  pad_width_output=((0, 0), (0, 0)),
+                                                                  pad_mode_output="symmetric",
+                                                                  pad_train_input=True,
+                                                                  pad_width_input=((0, 0), (0, 0)),
+                                                                  pad_mode_input="symmetric")
+
+
+
+Xtrain, Ytrain, Xtest, Ytest = Xtrain1, Ytrain1, Xtest1, Ytest1
+Xtrain, Ytrain, Xtest, Ytest = Xtrain0, Ytrain0, Xtest0, Ytest0
 # ############################# Kernel additive model (KAM) ############################################################
-Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_dataset(cca, rcst, n_train=N_TRAIN, normalize01=True,
-                                                              interpout=False, pad_train_input=False,
-                                                              pad_train_output=False)
+# Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_dataset(cca, rcst, n_train=N_TRAIN, normalize01=True,
+#                                                               interpout=False, pad_train_input=False,
+#                                                               pad_train_output=False)
+
+# Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_bis(cca.copy(), rcst.copy(), n_train=N_TRAIN,
+#                                                           normalize01=True, interpout=False, pad_train=True,
+#                                                           pad_width=((0, 0), (0, 0)), pad_mode="symmetric")
+
 domain_out = np.array([[0, 1]])
 nevals_in = 100
 nevals_out = 60
 nevals_fpca = 500
 
-dict_test = {"regu": 1e-3, "kx": 0.05, "ky": 0.05, "keval": 0.03, "nfpca": 30}
+dict_test = {'regu': 0.007564633275546291, 'kx': 0.1, 'ky': 0.05, 'keval': 0.1, 'nfpca': 30}
 reg = generate_expes.create_kam_dti(dict_test, nevals_in, nevals_out, domain_out, domain_out, nevals_fpca)
 reg.fit(Xtrain, Ytrain)
-test_pred = reg.predict_evaluate(Xtest, Ytest[0][0])
+len_test = len(Xtest[0])
+test_pred = [reg.predict_evaluate(([Xtest[0][i]], [Xtest[1][i]]), Ytest[0][i]) for i in range(len(Xtest[0]))]
+score = model_eval.mean_squared_error(test_pred, [Ytest[1][i] for i in range(len_test)])
 
 
 # ############################# Kernel estimator (KE) ##################################################################
@@ -58,24 +81,45 @@ test_pred = reg.predict_evaluate(Xtest, Ytest[0][0])
 
 # ############################# Kernel-based projection learning #######################################################
 domain_out = np.array([[0, 1]])
-pad_width = ((0, 0), (165, 165))
-domain_out_pad = np.array([[-pad_width[1][0] / 55, 1 + pad_width[1][0] / 55]])
-Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_dataset(cca, rcst, n_train=N_TRAIN, normalize01=True,
-                                                              interpout=False, pad_train_input=False,
-                                                              pad_train_output=True, pad_width_output=pad_width,
-                                                              pad_mode_output="symmetric")
-Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_bis(cca, rcst, n_train=N_TRAIN, normalize01=True,
-                                                          interpout=False, pad_train=True, pad_width=pad_width,
-                                                          pad_mode="symmetric")
+pad_width_output = ((0, 0), (55, 55))
+pad_width_input = ((0, 0), (0, 0))
+domain_out_pad = np.array([[-pad_width_output[1][0] / 55, 1 + pad_width_output[1][0] / 55]])
+# Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_dataset(cca, rcst, n_train=N_TRAIN, normalize01=True,
+#                                                               interpout=False, pad_train_input=False,
+#                                                               pad_train_output=True, pad_width_output=pad_width,
+#                                                               pad_mode_output="symmetric")
+# Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_bis(cca, rcst, n_train=N_TRAIN, normalize01=True,
+#                                                           interpout=False, pad_train=True, pad_width=pad_width,
+#                                                           pad_mode="symmetric")
+Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_dataset(cca.copy(), rcst.copy(), normalize01=True,
+                                                              pad_mode_input="symmetric",
+                                                              pad_width_input=pad_width_input,
+                                                              pad_mode_output="symmetric",
+                                                              pad_width_output=pad_width_output)
 Xtrain = np.array(Xtrain[1]).squeeze()
 Xtest = np.array(Xtest[1]).squeeze()
-# TODO : understand why there is a difference between the two processing (and take the one that works if we do not find)
 dict_test = {"regu": 0.009236708571873866, "ker_sigma": 0.9, "pywt_name": "db", "init_dilat": 1, "dilat": 2, "translat": 1,
              "moments": 2, "n_dilat": 5, "center_outputs": True,
-             "penalize_freqs": 1.2, "add_constant": True}
-reg = generate_expes.create_kpl_dti(dict_test, domain_out, domain_out_pad, pad_width)
+             "penalize_freqs": 1.0, "add_constant": True}
+reg = generate_expes.create_kpl_dti(dict_test, domain_out, domain_out_pad, pad_width_output)
 reg.fit(Xtrain, Ytrain)
 test_pred = reg.predict_evaluate_diff_locs(Xtest, Ytest[0])
 score = model_eval.mean_squared_error(test_pred, Ytest[1])
 
 # ############################# Triple basis estimator (3BE) ###########################################################
+domain_out = np.array([[0, 1]])
+nrffs = 300
+rffs_seed = 567
+pad_width = ((0, 0), (0, 0))
+
+Xtrain, Ytrain, Xtest, Ytest = processing.process_dti_dataset(cca.copy(), rcst.copy(), normalize01=True,
+                                                              pad_mode_output="symmetric", pad_width_output=pad_width)
+
+dict_test = {"regu": 1, "ker_sigma": 20, "max_freq_in": 25,
+             "max_freq_out": 5, "center_outputs": True}
+
+
+reg = generate_expes.create_3be_dti(dict_test, domain_out, domain_out, nrffs, rffs_seed, pad_width)
+reg.fit(Xtrain, Ytrain)
+test_pred = [reg.predict_evaluate(([Xtest[0][i]], [Xtest[1][i]]), Ytest[0][i]) for i in range(len(Xtest[0]))]
+score = model_eval.mean_squared_error(test_pred, [Ytest[1][i] for i in range(len_test)])
