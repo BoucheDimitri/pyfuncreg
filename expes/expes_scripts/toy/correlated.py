4fd0ee15924d44c34baf1dcfa216e92bb3b1bab0
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2020-03-20 10:49:41 +0100

    Config commit for cluster

--- expes/expes_scripts/toy/correlated.py
+++ expes/expes_scripts/toy/correlated.py
@@ -18,14 +18,14 @@ from model_eval import parallel_tuning
 
 # ############################### Config ###############################################################################
 # Record config
-OUTPUT_FOLDER = "toy"
-EXPE_NAME = "correlated"
+OUTPUT_FOLDER = "toy_correlated"
 # Shuffle seed
 SHUFFLE_SEED = 784
 INPUT_INDEXING = "array"
 OUTPUT_INDEXING = "discrete_general"
 N_FOLDS = 5
-N_PROCS = 8
+N_PROCS = None
+MIN_PROCS = 32
 
 # ############################### Regressor config #####################################################################
 REGU = np.geomspace(1e-8, 1, 200)
@@ -65,15 +65,17 @@ if __name__ == '__main__':
         Xtrain_deg = degradation.add_noise_inputs(Xtrain, NOISE_INPUT, SEED_INPUT)
         Ytrain_deg = degradation.add_noise_outputs(Ytrain, NOISE_OUTPUT, SEED_OUTPUT)
         best_config_corr, best_result_corr, score_test_corr = parallel_tuning.parallel_tuning(
-            regs_corr, Xtrain_deg, Ytrain_deg, Xtest, Ytest, rec_path=rec_path, key="corr_" + str(n_samples),
-            configs=configs_corr, n_folds=N_FOLDS, n_procs=N_PROCS,
-            input_indexing=INPUT_INDEXING, output_indexing=OUTPUT_INDEXING)
+            regs_corr, Xtrain_deg, Ytrain_deg, Xtest, Ytest, configs=configs_corr, n_folds=N_FOLDS, n_procs=N_PROCS,
+            min_nprocs=MIN_PROCS, input_indexing=INPUT_INDEXING, output_indexing=OUTPUT_INDEXING)
         best_config, best_result, score_test = parallel_tuning.parallel_tuning(
-            regs, Xtrain_deg, Ytrain_deg, Xtest, Ytest, rec_path=rec_path, key="non_corr_" + str(n_samples),
-            configs=configs, n_folds=N_FOLDS, n_procs=N_PROCS,
-            input_indexing=INPUT_INDEXING, output_indexing=OUTPUT_INDEXING)
+            regs, Xtrain_deg, Ytrain_deg, Xtest, Ytest, configs=configs, n_folds=N_FOLDS, n_procs=N_PROCS,
+            min_nprocs=MIN_PROCS, input_indexing=INPUT_INDEXING, output_indexing=OUTPUT_INDEXING)
         scores_test_corr.append(score_test_corr)
         scores_test.append(score_test)
+        with open(rec_path + "/" + str(n_samples) + "_corr.pkl", "wb") as out:
+            pickle.dump((best_config_corr, best_result_corr, score_test_corr), out, pickle.HIGHEST_PROTOCOL)
+        with open(rec_path + "/" + str(n_samples) + ".pkl", "wb") as out:
+            pickle.dump((best_config, best_result, score_test), out, pickle.HIGHEST_PROTOCOL)
 
     with open(rec_path + "/full.pkl", "wb") as out:
         pickle.dump((N_SAMPLES, scores_test, scores_test_corr), out, pickle.HIGHEST_PROTOCOL)
