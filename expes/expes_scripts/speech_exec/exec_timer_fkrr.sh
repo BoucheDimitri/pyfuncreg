6296daa425c5b3d3c28930fb7d50114ab01472b1
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2020-05-31 18:39:14 +0200

    Added scripts

---
+++ expes/expes_scripts/speech_exec/exec_timer_fkrr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python timer_fkrr.py LP
+python timer_fkrr.py LA
+python timer_fkrr.py TBCL
+python timer_fkrr.py TBCD
+python timer_fkrr.py VEL
+python timer_fkrr.py GLO
+python timer_fkrr.py TTCL
+python timer_fkrr.py TTCD
---
+++ expes/expes_scripts/speech_exec/exec_timer_kpl.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python timer_kpl.py LP
+python timer_kpl.py LA
+python timer_kpl.py TBCL
+python timer_kpl.py TBCD
+python timer_kpl.py VEL
+python timer_kpl.py GLO
+python timer_kpl.py TTCL
+python timer_kpl.py TTCD
