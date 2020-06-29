e4f8735d71258d8bde1d1486051a9257939f8144
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2020-06-29 12:32:28 +0200

    Commit for cluster

--- expes/expes_scripts/toy/output_missing_3be.py
+++ expes/expes_scripts/toy/output_missing_3be.py
@@ -41,6 +41,7 @@ MISSING_LEVELS = np.arange(0, 1, 0.05)
 # MISSING_LEVELS = [0, 0.1]
 DOMAIN_OUT = toy_data_spline.DOM_OUTPUT
 DOMAIN_IN = toy_data_spline.DOM_INPUT
+LOCS_IN = np.linspace(DOMAIN_IN[0, 0], DOMAIN_IN[0, 1], toy_data_spline.N_LOCS_INPUT)
 
 KIN_SIGMA = [0.05]
 N_FREQS_IN = [15]
@@ -85,10 +86,14 @@ if __name__ == '__main__':
     for i in range(N_AVERAGING):
         scores_dicts.append({})
         for n_samples in NSAMPLES_LIST:
-            Xtrain, Ytrain, Xtest, Ytest = toy_data_spline.get_toy_data(n_samples, seed=seeds_data[i])
+            Xtrain, Ytrain, Xtest, Ytest = toy_data_spline.get_toy_data(n_samples, seed=seeds_data[i],
+                                                                        squeeze_locs=True)
             scores_dicts[i][n_samples] = []
+            Xtest = ([LOCS_IN for j in range(Xtest.shape[0])], [Xtest[j] for j in range(Xtest.shape[0])])
             for deg in MISSING_LEVELS:
                 Xtrain_deg = degradation.add_noise_inputs(Xtrain, NOISE_INPUT, seeds_noise_in[i])
+                Xtrain_deg = (
+                [LOCS_IN for j in range(Xtrain_deg.shape[0])], [Xtrain_deg[j] for j in range(Xtrain_deg.shape[0])])
                 Ytrain_deg = degradation.add_noise_outputs(Ytrain, NOISE_OUTPUT, seeds_noise_out[i])
                 Ytrain_deg = degradation.downsample_output(Ytrain_deg, deg, seeds_missing[i])
                 best_config, best_result, score_test = parallel_tuning.parallel_tuning(
