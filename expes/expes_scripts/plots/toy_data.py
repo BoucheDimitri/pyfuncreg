11bc3eb1e39df39816a42003ad5a200a491f521c
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2020-06-29 12:17:23 +0200

    Commit for cluster

--- expes/expes_scripts/plots/toy_data.py
+++ expes/expes_scripts/plots/toy_data.py
@@ -16,11 +16,12 @@ plt.rcParams.update({"ytick.major.width": 2.5})
 
 n_samples = 100
 noise_input = 0.07
-noise_output = 0.02
+noise_output = 0.01
 Xtrain, Ytrain, Xtest, Ytest = toy_data_spline.get_toy_data(n_samples, seed=784)
 
 Xtrain = degradation.add_noise_inputs(Xtrain, noise_input, seed=678)
 Ytrain = degradation.add_noise_outputs(Ytrain, noise_output, seed=678)
+Ytrain = degradation.downsample_output(Ytrain, 0.5, 678)
 
 dom_in = np.linspace(toy_data_spline.DOM_INPUT[0, 0], toy_data_spline.DOM_INPUT[0, 1], Xtrain.shape[1])
 
@@ -28,16 +29,17 @@ fig, axes = plt.subplots(2, 4, sharey="row")
 for i in range(4):
     axes[0, i].plot(dom_in, Xtrain[i])
     axes[0, i].set_xlabel("$\gamma$")
-    axes[1, i].plot(Ytrain[0][i], Ytrain[1][i])
+    axes[1, i].scatter(Ytrain[0][i], Ytrain[1][i])
     axes[1, i].set_xlabel("$\\theta$")
 
 axes[0, 0].set_ylabel("$x(\gamma)$")
 axes[1, 0].set_ylabel("$y(\\theta)$")
 
-
-bs = BSpline.basis_element(t = (-0.5, -0.25, 0, 0.25, 0.5))
+t = np.array([0, 0.5, 1, 1.5, 2])
+for i in range(0, 10):
+    bs = BSpline.basis_element(t + i, extrapolate=False)
 # sp = np.linspace(0, 4, 200)
-sp = np.linspace(-0.5, 0.5, 200)
-plt.plot(sp, bs(sp))
-plt.xlabel("$\\theta$")
-plt.ylabel("$B_4(\\theta)$")
+    sp = np.linspace(0, 11, 500)
+    plt.plot(sp, bs(sp))
+    plt.xlabel("$\\theta$")
+    plt.ylabel("$\phi(\\theta)$")
