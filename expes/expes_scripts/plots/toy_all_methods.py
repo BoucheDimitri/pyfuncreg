138c7e66b47d2910ba8d9d5664e445f874682a35
BoucheDimitri <dimitri.bouche@telecom-paris.fr>
2020-06-30 18:19:01 +0200

    Added output noise plot for comparisons on toy data

--- expes/expes_scripts/plots/toy_all_methods.py
+++ expes/expes_scripts/plots/toy_all_methods.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import numpy as np
+import matplotlib.pyplot as plt
 from data import toy_data_spline
 
 path = "/home/dimitri/Desktop/Telecom/Outputs/all_outputs_30-06-2020_09-47/outputs/"
@@ -14,6 +15,7 @@ def consolidate_dicts(folder, path=path):
     return np.array(results)
 
 
+# ########################### OUTPUT MISSING ###########################################################################
 # folders = ["output_missing_kpl/", "output_missing_fkrr/", "output_missing_3be/"]
 # corresp = ["KPL", "FKRR", "3BE", "KAM"]
 folders = ["output_missing_kpl/", "output_missing_fkrr/", "output_missing_kam/"]
@@ -21,44 +23,40 @@ corresp = ["KPL", "FKRR", "KAM"]
 
 all_results = {corresp[i]: np.array(consolidate_dicts(folders[i])) for i in range(len(folders))}
 means = {key: np.mean(all_results[key], axis=0) for key in all_results.keys()}
+stds = {key: np.std(all_results[key], axis=0) for key in all_results.keys()}
+missing_levels = np.arange(0, 1, 0.05)
 
-
-
-def get_mean_std(folder, n_samp)
-
-
-with open(path + "full.pkl", "rb") as inp:
-    missing_levels, big_list = pickle.load(inp)
-
-n_samples = list(big_list[0].keys())
-
-array_dict = {n: [] for n in n_samples}
-means_dict = {}
-stds_dict = {}
-
-for n in n_samples:
-    for i in range(len(big_list)):
-        array_dict[n].append(big_list[i][n])
-
-for n in n_samples:
-    array_dict[n] = np.array(array_dict[n])
-
-for n in n_samples:
-    stds_dict[n] = array_dict[n].std(axis=0)
-    means_dict[n] = array_dict[n].mean(axis=0)
-
-sub_n_samples = [10, 50, 100, 500]
 markers = ["o", "v", "s", "D"]
 count = 0
-for n in sub_n_samples:
-    # plt.plot(missing_levels, means_dict[n], label="N=" + str(n))
-    plt.errorbar(100 * np.array(missing_levels), means_dict[n],
-                 yerr=stds_dict[n], marker=markers[count], capsize=5, label="N=" + str(n), lolims=True)
+for key in corresp:
+    plt.errorbar(100 * np.array(missing_levels), means[key],
+                 yerr=stds[key], marker=markers[count], capsize=5, label=key, lolims=True)
     count += 1
 
-plt.legend()
-plt.ylabel("MSE")
-plt.xlabel("Percentage missing")
 
-path = "/home/dimitri/Desktop/Telecom/Outputs/all_outputs_30-06-2020_09-47/outputs/"
+# ########################### OUTPUT NOISE #############################################################################
+folders = ["output_noise_kpl/", "output_noise_fkrr/"]
+corresp = ["KPL", "FKRR"]
 
+all_results = {corresp[i]: np.array(consolidate_dicts(folders[i])) for i in range(len(folders))}
+means = {key: np.mean(all_results[key], axis=0) for key in all_results.keys()}
+stds = {key: np.std(all_results[key], axis=0) for key in all_results.keys()}
+noise_levels = np.linspace(0, 1.5, 50)
+
+# Estimate mean absolute signal
+Xtrain, Ytrain, Xtest, Ytest = toy_data_spline.get_toy_data(5000)
+mean_abs_signal = np.mean(np.abs(np.array(Ytrain[1])))
+
+if noise_levels[0] == 0:
+    beg_ind = 1
+else:
+    beg_ind = 0
+snr_grid = np.flip((mean_abs_signal / noise_levels[beg_ind:]))
+
+markers = ["o", "v", "s", "D"]
+count = 0
+for key in corresp:
+    plt.plot(snr_grid, np.flip(means[key][beg_ind:]), label=key, marker=markers[count])
+    count += 1
+
+plt.yscale("log")
